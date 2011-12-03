4088fc97b00b9a55f576e257a19898d7c27f82b2
Praveen Kumar <praveen+git@kumar.in>
2011-12-02 22:17:25 -0800

    Add comment highlighting capbaility to ms-utest-mode.

--- custom-lisp/ms-utest-mode.el
+++ custom-lisp/ms-utest-mode.el
@@ -14,7 +14,17 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (defcustom ms-utest-mode-hook nil
-  "Hook run when entering MS UTest mode")
+  "Hook run when entering `ms-utest-mode'"
+  )
+
+(add-to-list 'auto-mode-alist (cons (purecopy "utest.txt") 'ms-utest-mode))
+
+(defvar ms-utest-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?# "<" table)
+    (modify-syntax-entry ?\n ">" table)
+    table)
+  "Syntax table for `ms-utest-mode'.")
 
 (defvar ms-utest-font-lock-keywords
   `((,(rx line-start
@@ -24,11 +34,17 @@
      (1 font-lock-keyword-face)
      (2 font-lock-function-name-face)
      (3 font-lock-comment-face))
-    ))
+    )
+  "Font lock keywords for `ms-utest-mode'."
+  )
 
-(define-derived-mode ms-utest-mode nil "MS-UTest"
-  "A major mode for editing Messaging Server unit test files"
+(define-derived-mode ms-utest-mode fundamental-mode "MS-UTest"
+  "A major mode for editing Messaging Server unit test files."
+  :syntax-table ms-utest-mode-syntax-table
   (set (make-local-variable 'font-lock-defaults)
-       '(ms-utest-font-lock-keywords)))
+       '(ms-utest-font-lock-keywords))
+  (set (make-local-variable 'comment-start) "# ")
+  (set (make-local-variable 'comment-start-skip) "#+[\t ]*")
+  )
 
 (provide 'ms-utest-mode)
