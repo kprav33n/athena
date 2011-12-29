cb81f218ca783b6448f8fb07bd73de5b2a31f2e3
Praveen Kumar <praveen+git@kumar.in>
2011-12-28 21:47:24 -0800

    Remove Oracel MS C style and add a unit test helper function.

--- modules/athena-programming-ms.el
+++ modules/athena-programming-ms.el
@@ -13,30 +13,20 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; UCS MS C/C++ style.
-;; Fixme: Change to auto detect stuff.
-(c-add-style
- "oracle-ucs-ms"
- '((indent-tabs-mode . t)
-   (fill-column . 75)
-   (c-basic-offset . 4)
-   (c-offsets-alist . ((innamespace . 0)
-                       (member-init-intro . ++)
-                       (access-label . /)
-                       ))))
-
-;; For msg source, use UCS MS style.
-(defun oracle-ucs-ms-style-hook ()
-  (when (and buffer-file-name
-             (string-match "/msg/" buffer-file-name))
-    (c-set-style "oracle-ucs-ms")))
-(add-hook 'c-mode-common-hook 'oracle-ucs-ms-style-hook 'append)
-
 ;; Guess style from the current code.
 (add-hook 'c-mode-common-hook 'guess-style-guess-all 'append)
 
 ;; Messaging Server unit test mode.
 (require 'ms-utest-mode)
 
+;; Print number of literals in region. This takes the 'C: ' prefix in
+;; the utest.txt into account. To count, select the region beginning
+;; at begin of the line of APPEND literals and ending at end of the
+;; line of APPEND data. Exclude the final <CR><LF> line.
+(defun ms-utest-count-literals-in-region (start end)
+  "Print number of literals in region."
+  (interactive "r")
+  (message "Region has %d literals"
+           (+ (- (- end start) (* (count-lines start end) 2)) 2)))
 
 (provide 'athena-programming-ms)
