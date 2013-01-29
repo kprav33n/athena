7eb4cfc67f4b81efb53d56c170dd5895e49efd75
Praveen Kumar <praveen+git@kumar.in>
2013-01-29 08:11:54 -0800

    Run pyflakes on-the-fly for Python files using flymake.

--- modules/athena-programming.el
+++ modules/athena-programming.el
@@ -97,7 +97,21 @@
 
 ;; Flymake.
 (require 'flymake)
+(add-hook 'find-file-hook 'flymake-find-file-hook)
 (setq flymake-gui-warnings-enabled nil)
+
+;; PyFlakes for python syntax checking.
+(defun flymake-pyflakes-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "pyflakes" (list local-file))))
+
+(add-to-list 'flymake-allowed-file-name-masks
+             '(".+\\.py$" flymake-pyflakes-init))
+
 
 ;; Helm gtags.
 (require 'helm-gtags)
