7fd7a07c09fa02dafcb3db23abe85c393db6fa78
Praveen Kumar <praveen+git@kumar.in>
2013-02-15 18:33:33 -0800

    Use flakes8 for Python flymake.

--- modules/athena-programming.el
+++ modules/athena-programming.el
@@ -145,6 +145,7 @@
 
 (require 'flymake-python-pyflakes)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+(setq flymake-python-pyflakes-executable "flake8")
 
 
 ;; iPython notebook support.
